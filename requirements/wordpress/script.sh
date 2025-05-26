25bb0226998605e57c1f24a50a77895b14f699fb
MasterCode_MFE <139508718+MasterCodeMFE@users.noreply.github.com>
2025-05-26 19:51:14 +0200

    todos los contenedores conectados

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -4,8 +4,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=manufern --admin_password=admin --admin_email=admin@admin.com --allow-root
-
-./wp-cli.phar user create manuel manuel@example.com --user_pass=Manufern --role=subscriber --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
 php-fpm8.2 -F
